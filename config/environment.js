313160d609dc8695339138d260258585775f7fc0
Modestas <modestask97@gmail.com>
2018-02-02 15:31:40 +0000

    created jwt token and login + register authentication

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 module.exports = {
   port: process.env.PORT || 4000,
   env: process.env.NODE_ENV,
+  secret: '123456789',
   db: {
     production: process.env.MONGODB_URI,
     development: 'mongodb://localhost/wdi-group-project-development',
---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { secret } =
+require('../config/environment');
+
+function register(req, res, next) {
+  User
+    .create(req.body)
+    .then(() => res.json({ message: 'Registration successful'}))
+    .catch(next);
+}
+
+function login(req, res, next) {
+  User
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if(!user || !user.validatePassword(req.body.password)) return res.unauthorized();
+
+      const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '1hr' });
+
+      // Generate a JWT and send it to the user as part of the JSON payload
+      return res.json({ token, message: `Welcome back ${user.username}` });
+    })
+    .catch(next);
+}
+
+module.exports = {
+  register,
+  login
+};
