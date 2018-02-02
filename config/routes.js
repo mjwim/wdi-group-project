cf833fd53eff7a4b81744d3ae7f2a1572612fd31
Matthew Wallis <mattjwallis@gmail.com>
2018-02-02 15:00:12 +0000

    login-register-routes-created

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router  = express.Router();
+const auth = require('../controllers/auth');
 
 // Routes go here
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
+
+router.all('/*', (req, res) => res.notFound());
 
 module.exports = router;
