1a2cb9259731ec0650a68ebea48de1d331350d14
Modestas <modestask97@gmail.com>
2018-02-05 15:26:53 +0000

    finished edit trip angular controller

--- src/js/controllers/trips/edit.controller.js
+++ src/js/controllers/trips/edit.controller.js
@@ -2,8 +2,18 @@ angular
   .module('tripsApp')
   .controller('TripsEditCtrl', TripsEditCtrl);
 
-TripsEditCtrl.$inject = ['Trip'];
-function TripsEditCtrl(trip) {
-  const vm = this
-  
+TripsEditCtrl.$inject = ['Trip', '$state'];
+function TripsEditCtrl(Trip, $state) {
+  const vm = this;
+
+  vm.trip = Trip.get($state.params);
+  vm.update = update;
+
+  function update() {
+    Trip
+      .update({ id: $state.params.id }, vm.trip)
+      .$promise
+      .then(() => $state.go('tripsIndex'));
+  }
+
 }
