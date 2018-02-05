44581c99ca999f4fbabffd444044ec901b32a7bd
Matthew Wallis <mattjwallis@gmail.com>
2018-02-05 11:08:38 +0000

    testing dependencies added

---
+++ test/api/trips_test.js
@@ -0,0 +1,23 @@
+/* globals api, expect, it, describe, afterEach, beforeEach */
+
+require('../spec_helper');
+
+describe('Trips Controller Test', () => {
+  describe('GET /api/trips', () => {
+    it('should return a 200 response', done => {
+      api
+        .get('/api/shoes')
+        .set('Accept', 'application/json')
+        .expect(200, done);
+    });
+    it('should return an array of trips', function(done) {
+      api
+        .get('/api/trips')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          expect(res.body).to.be.an('array');
+          done();
+        });
+    });
+  });
+});
