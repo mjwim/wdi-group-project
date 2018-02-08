f949e3ebe24a94148dca7d5a4089652c16fa10a7
Modestas <modestask97@gmail.com>
2018-02-08 10:45:39 +0000

    implemented filter for index page for filtering by name but does not load trips on page load

--- src/js/controllers/trips/index.controller.js
+++ src/js/controllers/trips/index.controller.js
@@ -2,18 +2,23 @@ angular
   .module('tripsApp')
   .controller('TripsIndexCtrl', TripsIndexCtrl);
 
-TripsIndexCtrl.$inject = ['Trip', 'filterFilter'];
-function TripsIndexCtrl(Trip, filterFilter) {
+TripsIndexCtrl.$inject = ['Trip', 'filterFilter', '$scope'];
+function TripsIndexCtrl(Trip, filterFilter, $scope) {
   const vm = this;
   vm.all = Trip.query();
 
+  
+
   function filterTrips() {
   // paramteres which you're filtering by
-    const params = { name: vm.searchBoxValue };
+    const params = { location: {name: vm.searchBoxValue}};
     // first argument what you are filtering
     // second is what you're filtering with
     vm.filtered = filterFilter(vm.all, params);
   }
 
-  filterTrips();
+
+  $scope.$watchGroup([
+    () => vm.searchBoxValue
+  ], filterTrips);
 }
